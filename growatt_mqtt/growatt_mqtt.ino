5db16b965fc586753ba3f46ef8e1d80dc4bb1d8c
Lukas de Boer <lukas.deboer@takeaway.com>
2019-09-16 18:09:28 +0200

    base version of wp

--- growatt_mqtt/growatt_mqtt.ino
+++ growatt_mqtt/growatt_mqtt.ino
@@ -9,18 +9,19 @@
 /******************************************************************
 Secrets, please change these in the secrets.h file
 *******************************************************************/
-const char* wifi_ssid     = WIFI_SSID;
-const char* wifi_password = WIFI_PASSWORD;
-const char* wifi_hostname = WIFI_HOSTNAME;
-const char* ota_password  = OTA_PASSWORD;
+const char* wifi_ssid                  = WIFI_SSID;
+const char* wifi_password              = WIFI_PASSWORD;
+const char* wifi_hostname              = WIFI_HOSTNAME;
+const char* ota_password               = OTA_PASSWORD;
 
-const char* mqtt_server   = MQTT_SERVER;
-const int   mqtt_port     = MQTT_PORT;
-const char* mqtt_username = MQTT_USERNAME;
-const char* mqtt_password = MQTT_PASSWORD;
+const char* mqtt_server                = MQTT_SERVER;
+const int   mqtt_port                  = MQTT_PORT;
+const char* mqtt_username              = MQTT_USERNAME;
+const char* mqtt_password              = MQTT_PASSWORD;
 
-const char* mqtt_topic_base = MQTT_TOPIC_BASE;
-const char* mqtt_logtopic = MQTT_LOGTOPIC;
+const char* mqtt_topic_base            = MQTT_TOPIC_BASE;
+const char* mqtt_logtopic              = MQTT_LOGTOPIC;
+const char* mqtt_set_temperature_topic = MQTT_SET_TEMPERATURE_TOPIC;
 
 /******************************************************************
 Useful for debugging, outputs info to a separate mqtt topic
@@ -34,6 +35,8 @@ char mqtt_topic[256];
 Instantiate modbus and mqtt libraries
 *******************************************************************/
 ModbusMaster node;
+int slave_id_growatt = 1;
+int slave_id_wp = 4;
 WiFiClient mqtt_wifi_client;
 PubSubClient mqtt_client(mqtt_wifi_client);
 
@@ -48,15 +51,36 @@ void log_message(char* string)
   } 
 }
 
+/******************************************************************************************
+ Callback function that is called when a message has been pushed to one of your topics.
+*******************************************************************************************/
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  char msg[length + 1];
+  for (int i=0; i < length; i++) {
+    msg[i] = (char)payload[i];
+  }
+  msg[length] = '\0';
+
+  if (strcmp(topic, mqtt_set_temperature_topic) == 0)
+  {
+    String temperature_string(msg);
+    float temperature = temperature_string.toFloat();
+    uint16_t temp_converted = (uint16_t)(temperature * 100);
+    sprintf(log_msg, "set temperature to %.02f (%d)", temperature, temp_converted); log_message(log_msg);
+
+    // instantiate modbusmaster with slave id wp
+    node.begin(slave_id_wp, Serial);
+    node.writeSingleRegister(54, temp_converted);
+  }
+}
+
+
 /******************************************************************
 Setup, only ran on application start
 *******************************************************************/
 void setup() {  
   Serial.begin(9600);
-  
-  // instantiate modbusmaster with slave id 1 (growatt)
-  node.begin(1, Serial);
-  
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_ssid, wifi_password);
   WiFi.hostname(wifi_hostname);
@@ -88,6 +112,7 @@ void setup() {
   ArduinoOTA.begin();
 
   mqtt_client.setServer(mqtt_server, mqtt_port);
+  mqtt_client.setCallback(mqtt_callback);
 }
 
 /******************************************************************
@@ -112,14 +137,24 @@ void publishFloat(char * topic, float f) {
   char value_char[32] = "";
   value_str.toCharArray(value_char, 40);
 
-  String topic_str = "inverter/" + String(topic);
-  char topic_char[128] = "";
-  topic_str.toCharArray(topic_char, 128);
   sprintf(mqtt_topic, "%s/%s", mqtt_topic_base, topic);
   
   mqtt_client.publish(mqtt_topic, value_char); 
 }
 
+/******************************************************************
+Publish int var to specified sub topic
+*******************************************************************/
+void publishInt(char * topic, int i) {
+  String value_str = String(i);
+  char value_char[32] = "";
+  value_str.toCharArray(value_char, 40);
+  
+  sprintf(mqtt_topic, "%s/%s", mqtt_topic_base, topic);
+  
+  mqtt_client.publish(mqtt_topic, value_char); 
+}
+   
 /******************************************************************
 Read values from modbus registers and output to seperate mqtt topics
 *******************************************************************/
@@ -136,36 +171,93 @@ void update_growatt() {
   char value[40] = "";
 
   if(millis() > next_poll){
-    log_message("Trying to read..");
+    next_poll = millis() + 10000;
+
+    log_message("Trying to read inverter..");
     
+    // instantiate modbusmaster with slave id growatt
+    node.begin(slave_id_growatt, Serial);
+  
     result = node.readInputRegisters(0, 32);
     // do something with data if read is successful
     if (result == node.ku8MBSuccess){
       log_message("success!");
-      publishFloat("status",float(node.getResponseBuffer(0)));
+      publishInt("energy/growatt/status", node.getResponseBuffer(0));
       
-      publishFloat("Ppv", glueFloat(node.getResponseBuffer(1), node.getResponseBuffer(2)));    
-      publishFloat("Vpv1", glueFloat(0, node.getResponseBuffer(3)));    
-      publishFloat("PV1Curr", glueFloat(0, node.getResponseBuffer(4)));    
-      publishFloat("Pac", glueFloat(node.getResponseBuffer(11), node.getResponseBuffer(12)));
-      publishFloat("Fac", glueFloat(0, node.getResponseBuffer(13))/10 );  
+      publishFloat("energy/growatt/Ppv", glueFloat(node.getResponseBuffer(1), node.getResponseBuffer(2)));    
+      publishFloat("energy/growatt/Vpv1", glueFloat(0, node.getResponseBuffer(3)));    
+      publishFloat("energy/growatt/PV1Curr", glueFloat(0, node.getResponseBuffer(4)));    
+      publishFloat("energy/growatt/Pac", glueFloat(node.getResponseBuffer(11), node.getResponseBuffer(12)));
+      publishFloat("energy/growatt/Fac", glueFloat(0, node.getResponseBuffer(13))/10 );  
 
-      publishFloat("Vac1", glueFloat(0, node.getResponseBuffer(14)));  
-      publishFloat("Iac1", glueFloat(0, node.getResponseBuffer(15)));
-      publishFloat("Pac1", glueFloat(node.getResponseBuffer(16), node.getResponseBuffer(17)));
+      publishFloat("energy/growatt/Vac1", glueFloat(0, node.getResponseBuffer(14)));  
+      publishFloat("energy/growatt/Iac1", glueFloat(0, node.getResponseBuffer(15)));
+      publishFloat("energy/growatt/Pac1", glueFloat(node.getResponseBuffer(16), node.getResponseBuffer(17)));
 
-      publishFloat("Etoday", glueFloat(node.getResponseBuffer(26), node.getResponseBuffer(27)));
-      publishFloat("Etotal", glueFloat(node.getResponseBuffer(28), node.getResponseBuffer(29)));
-      publishFloat("ttotal", glueFloat(node.getResponseBuffer(30), node.getResponseBuffer(31)));
-      publishFloat("Tinverter", glueFloat(0, node.getResponseBuffer(32)));
+      publishFloat("energy/growatt/Etoday", glueFloat(node.getResponseBuffer(26), node.getResponseBuffer(27)));
+      publishFloat("energy/growatt/Etotal", glueFloat(node.getResponseBuffer(28), node.getResponseBuffer(29)));
+      publishFloat("energy/growatt/ttotal", glueFloat(node.getResponseBuffer(30), node.getResponseBuffer(31)));
+      publishFloat("energy/growatt/Tinverter", glueFloat(0, node.getResponseBuffer(32)));
     } else {
       tmp = String(result, HEX);
       tmp.toCharArray(value, 40);
       
       log_message("error!");
       log_message(value);
+      publishInt("energy/growatt/status", -1);
     }
-    next_poll = millis() + 10000;
+    node.clearResponseBuffer();
+    log_message("Trying to read wp input registers..");
+    
+    // instantiate modbusmaster with slave id wp
+    node.begin(slave_id_wp, Serial);
+    
+    result = node.readInputRegisters(1, 120);
+    if (result == node.ku8MBSuccess) {
+      log_message("success!");
+      publishInt("heating/mitsubishi/status", 1);
+
+      publishInt("heating/mitsubishi/defrost", node.getResponseBuffer(26)); // 0 = Normal, 1 = Standby, 2 = Defrost, 3 = Waiting Restart
+      publishFloat("heating/mitsubishi/zone1_temperature_setpoint", node.getResponseBuffer(40) / 100.0);
+      publishFloat("heating/mitsubishi/zone1_flow_temperature_setpoint", node.getResponseBuffer(44) / 100.0);
+      
+      publishFloat("heating/mitsubishi/legionella_temperature_setpoint", node.getResponseBuffer(48) / 100.0);
+
+      publishFloat("heating/mitsubishi/dhw_temperature_drop", node.getResponseBuffer(50) / 10.0);
+    
+      publishFloat("heating/mitsubishi/zone1_room_temperature", node.getResponseBuffer(52) / 100.0);
+
+      publishFloat("heating/mitsubishi/refrigerant_temperature", node.getResponseBuffer(56) / 100.0);
+      publishFloat("heating/mitsubishi/outdoor_ambient_temperature", node.getResponseBuffer(58) / 10.0);
+      
+      publishFloat("heating/mitsubishi/water_outlet_temperature", node.getResponseBuffer(60) / 100.0);
+      publishFloat("heating/mitsubishi/water_inlet_temperature", node.getResponseBuffer(62) / 100.0);
+      
+      
+    } else {
+      tmp = String(result, HEX);
+      tmp.toCharArray(value, 40);
+      log_message("error!");
+      log_message(value);
+      publishInt("heating/mitsubishi/status", -1);
+    }
+
+    node.clearResponseBuffer();
+
+    log_message("Trying to read wp holding registers..");
+    result = node.readHoldingRegisters(4, 120);
+    if (result == node.ku8MBSuccess) {
+      log_message("success!");
+//      publishInt("heating/mitsubishi/operatingmode", node.getResponseBuffer(26)); // 0 = stop, 1 = hot water, 2 = heat, 3 = Cooling, 5 = Freeze Stat, , 6 = Legionella, 7 = Heating-Eco
+    
+    } else {
+      tmp = String(result, HEX);
+      tmp.toCharArray(value, 40);
+      log_message("error!");
+      log_message(value);
+    }
+    node.clearResponseBuffer();
+
   }
 }
 
@@ -177,10 +269,13 @@ void mqtt_reconnect()
   // Loop until we're reconnected
   while (!mqtt_client.connected()) 
   {
-    if (!mqtt_client.connect(wifi_hostname, mqtt_username, mqtt_password)) {
+    if (mqtt_client.connect(wifi_hostname, mqtt_username, mqtt_password)) {
+      mqtt_client.subscribe(mqtt_set_temperature_topic);
+    } else {
       // Wait 5 seconds before retrying
       delay(5000);
     }
+
   }
 }
 
